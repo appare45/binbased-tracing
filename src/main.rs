3ec0a46573f24593c4de7f5f3e1ab314bae0424e
appare45 <kaibatsu35.7m45@gmail.com>
2026-01-17 19:18:58 +0900

    with args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(value_name = "PID")]
+    pid: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Target PID: {}", args.pid);
 }
