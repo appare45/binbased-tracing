c6fcd0c1568517226a3cc783c77a476bf8cd48a6
appare45 <kaibatsu35.7m45@gmail.com>
2026-01-22 12:59:07 +0900

    mmap

--- src/instrument.rs
+++ src/instrument.rs
@@ -1,8 +1,8 @@
-use crate::{error::InstrumentError, ptrace};
+use crate::{error::InstrumentError, proc, ptrace};
 
 pub enum Instrument {
     NotInstrumented(ptrace::Tracee),
-    PreInstrumented(ptrace::Tracee),
+    PreInstrumented(ptrace::Tracee, u64),
     Instrumented(ptrace::Tracee),
 }
 
@@ -19,33 +19,44 @@ impl Instrument {
     pub fn pre_instrument(self) -> Result<Instrument, InstrumentError> {
         match self {
             Instrument::NotInstrumented(tracee) => {
+                let base = tracee.base().unwrap_or(0);
+                println!("Base: {base:x}");
                 let instructions = build_mmap();
 
                 let tracee = tracee.stop()?;
-                let regs = tracee.get_regs()?;
-                let pc = regs.pc;
-                let saved = tracee.write_instructions(pc, &instructions)?;
+                let saved_regs = tracee.get_regs()?;
+                let pc = saved_regs.pc;
+                let mut regs = saved_regs.clone();
+                let params = [
+                    base,
+                    1024,     // Size
+                    3,        // PROT_READ | PROT_WRITE
+                    0x22,     // MAP_PRIVATE | MAP_ANONYMOUS
+                    u64::MAX, // -1
+                ];
+                for (i, v) in params.iter().enumerate() {
+                    regs.regs[i] = *v;
+                }
+                regs.regs[8] = 222;
+                tracee.set_regs(regs)?;
+                let saved = tracee.write(pc, &instructions)?;
 
                 let tracee = tracee.cont()?.wait()?;
-                tracee.write_instructions(pc, &saved)?;
-                tracee.set_regs(regs)?;
+                let addr = tracee.get_regs()?.regs[0];
+                println!("Allocated at 0x{addr:x}");
+                tracee.write(pc, &saved)?;
+                tracee.set_regs(saved_regs)?;
 
-                Ok(Instrument::PreInstrumented(tracee))
+                Ok(Instrument::PreInstrumented(tracee, addr))
             }
             _ => Err(InstrumentError::AlreadyPreInstrumented),
         }
     }
 }
 
-// 一旦Xと出力するシステムコールを呼ぶだけのコードを生成する
-// 元のアセンブリはasm.sを参照
+// トランポリンコードを置くためのmmapを呼び出す
 fn build_mmap() -> Vec<i64> {
     let mut buf = Vec::new();
-    // as -o asm.o asm.s && objdump -d asm.o
-    buf.push(0x52800820 | 0x381f0fe0 << 32);
-    buf.push(0xd2800020 | 0x910003e1 << 32);
-    buf.push(0xd2800022 | 0xd2800808 << 32);
-    buf.push(0xd4000001 | 0x910043ff << 32);
-    buf.push(0xd4200000);
+    buf.push(0xd4000001u64 as i64 | 0xd4200000 << 32); // svc #0; brk #0
     return buf;
 }
